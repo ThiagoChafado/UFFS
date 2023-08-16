41828c8c065bce5bb2033f371a419787ac1b44fa
ThiagoChafado <thiagochafado123@gmail.com>
2023-08-16 08:55:37 -0300

    new leetcode folder

---
+++ LeetCode/1.twosums.cpp
@@ -0,0 +1,25 @@
+
+using namespace std;
+#include<vector>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap[nums[i]] = i;
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement) && numMap[complement] != i) {
+                return {i, numMap[complement]};
+            }
+        }
+
+        return {}; // No solution found
+    }
+};
---
+++ LeetCode/README.md
@@ -0,0 +1,1 @@
+Esse repositório contem apenas as funções de exercicios resolvidos do leetcode, sem a funcao main
